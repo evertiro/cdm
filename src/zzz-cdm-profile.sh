f3ce43e492cb8d7b2ab163240b45dadea403b0d7
Ghost1227 <ghost1227@archlinux.us>
2010-01-03 21:33:53 -0600

    Massive update. See CHANGELOG for details.

--- src/zzz-cdm-profile.sh
+++ src/zzz-cdm-profile.sh
@@ -1,9 +1,7 @@
 if [[ -z "$DISPLAY" ]] && [[ $(tty) = /dev/tty1 ]]; then
 	# Drop root to console
 	if [[ $EUID -eq 0 ]]; then
-		${SHELL}
-		exit 0
+		exec ${SHELL}
 	fi
-	cdm
-	exit 0
+	exec cdm
 fi
