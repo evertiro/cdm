1df217f0a53df4087ebefa54425298bc752a3ba5
Ghost1227 <ghost1227@archlinux.us>
2011-01-02 17:42:28 -0600

    Updated to 0.5.3 read CHANGELOG for details

--- src/zzz-cdm-profile.sh
+++ src/zzz-cdm-profile.sh
@@ -1,8 +1,9 @@
 if [[ -z "$DISPLAY" && -z "$SSH_TTY" && $(tty) = /dev/tty* ]]; then
 	# Drop root to console
-	if [[ $EUID -eq 0 ]]; then
-		exec ${SHELL}
+	if ! [[ $EUID -eq 0 ]]; then
+		cdm
+		if (( $? == 0 )); then
+			exit 0
+		fi
 	fi
-	cdm
-	exit 0
 fi
