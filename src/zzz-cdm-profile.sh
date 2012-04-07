4b0e9825ebc50525550c9d499244e680d06b0a94
Casper Ti. Vector <CasperVector@gmail.com>
2012-04-07 19:37:34 +0800

    Major restructuring. classify menu items into console programs and X programs. Console programs are directly `exec'ed. X programs are run with cdm-xlaunch(1). $allowconsole, $usexinit, shutdownmenu(), etc., are removed.

--- src/zzz-cdm-profile.sh
+++
@@ -1,9 +0,0 @@
-if [[ -z "$DISPLAY" && -z "$SSH_TTY" && $(tty) = /dev/tty* ]]; then
-	# Drop root to console
-	if ! [[ $EUID -eq 0 ]]; then
-		cdm
-		if (( $? == 0 )); then
-			exit 0
-		fi
-	fi
-fi
