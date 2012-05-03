510652d15280b2be1e02d4fdda7dc101e415efe9
Casper Ti. Vector <CasperVector@gmail.com>
2012-05-04 00:30:28 +0800

    Again replace '` ... `' with '$( ... )'.

--- src/profile.sh
+++ src/profile.sh
@@ -1,5 +1,5 @@
 if [[ "$(tty)" == /dev/tty1 ]]; then
 	[[ -n "$CDM_SPAWN" ]] && return
-	[[ -z "$DISPLAY`pgrep xinit`$SSH_TTY" ]] && exec cdm
+	[[ -z "$DISPLAY$SSH_TTY$(pgrep xinit)" ]] && exec cdm
 fi
 
