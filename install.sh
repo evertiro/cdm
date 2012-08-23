6d05770aca091dd62ab660d82fbfc1741241aa70
Casper Ti. Vector <CasperVector@gmail.com>
2012-08-23 12:45:26 +0800

    Update install.sh (forgot for a long time :|).

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 install -d /usr/bin/ /etc/ /usr/share/doc/cdm
-install -Dm755 cdm cdm-xlaunch /usr/bin/
-install -Dm644 cdmrc /etc/cdmrc
+install -Dm755 src/cdm src/cdm-xlaunch /usr/bin/
+install -Dm644 src/cdmrc /etc/cdmrc
 install -Dm644 themes /usr/share/doc/cdm
-install -Dm644 README ChangeLog* /usr/share/doc/cdm
+install -Dm644 README ChangeLog* src/profile.sh themes /usr/share/doc/cdm
 
