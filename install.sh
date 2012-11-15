6250d5bf83fd576684ab377ae4bbb2f4f3900c3a
Casper Ti. Vector <CasperVector@gmail.com>
2012-11-15 22:39:01 +0800

    install.sh: README -> README.md

--- install.sh
+++ install.sh
@@ -3,6 +3,6 @@
 install -d /usr/bin /etc /usr/share/doc/cdm/themes
 install -Dm755 src/cdm src/cdm-xlaunch /usr/bin
 install -Dm644 src/cdmrc /etc/cdmrc
-install -Dm644 README src/profile.sh /usr/share/doc/cdm
+install -Dm644 README.md src/profile.sh /usr/share/doc/cdm
 install -Dm644 themes/* /usr/share/doc/cdm/themes
 
