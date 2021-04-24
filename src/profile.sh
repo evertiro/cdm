82cea11ff36ade0dcdaa245443d7f61e22c60ba5
Dan Griffiths <dgriffiths@widgit.io>
2021-04-24 12:40:22 -0400

    Always specify the interpreter

--- src/profile.sh
+++ src/profile.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 # To avoid potential situation where cdm(1) crashes on every TTY, here we
 # default to execute cdm(1) on tty1 only, and leave other TTYs untouched.
 if [[ "$(tty)" == '/dev/tty1' ]]; then
